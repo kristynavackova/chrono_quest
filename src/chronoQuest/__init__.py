05afdb857c09f313910111f9e943c9affb90501c
ReviOrion <kishin.orion@gmail.com>
2025-09-02 13:26:29 +0200

    Refactor view configurations to use relative template paths and add static view configuration

--- src/chronoQuest/__init__.py
+++ src/chronoQuest/__init__.py
@@ -3,10 +3,20 @@ import mimetypes
 
 
 def main(global_config=None, **settings):
+    # Add support for WebP images
     mimetypes.add_type('image/webp', '.webp', strict=True)
+
+    # Disable template caching during development (to be removed in production)
+    # Na pythonAnywhere.com není auto-reload šabllon potřeba.
+    settings['pyramid.reload_templates'] = False
+
+    # Create Pyramid WSGI application
     with Configurator(settings=settings) as config:
         config.include("pyramid_jinja2")
+        # Statické soubory (CSS, JS, obrázky)
         config.add_static_view(name='static', path='chronoQuest:static', cache_max_age=86400)
+        # Definice rout (URI) a jejich mapování na view funkce
+        # The root URL redirects to /home
         config.add_route("root","/")
         config.add_route("home","/home")
         config.add_route('themes','/themes')
--- src/chronoQuest/views.py
+++ src/chronoQuest/views.py
@@ -6,7 +6,7 @@ def root_view(request):
     return HTTPFound(location=request.route_url('home'))
 
 
-@view_config(route_name='home', renderer='C:\\Users\\Martin\\Desktop\\ŠR 25-26\\Tools\\ChronoQuest\\chrono_quest\\src\\chronoQuest\\templates\\home.jinja2')
+@view_config(route_name='home', renderer='chronoQuest:templates/home.jinja2')
 def home_view(request):
     return {
         "title": "ChronoQuest - úvod",
@@ -18,12 +18,12 @@ def home_view(request):
         ],
     }
 
-@view_config(route_name='login', renderer='C:\\Users\\Martin\\Desktop\\ŠR 25-26\\Tools\\ChronoQuest\\chrono_quest\\src\\chronoQuest\\templates\\login.jinja2', request_method=('GET','POST'))
+@view_config(route_name='login', renderer='chronoQuest:templates/login.jinja2', request_method=('GET','POST'))
 def login_view(request):
     # TODO: zde později zpracujeme POST (ověření uživatele, session, redirect)
     return {"title": "Přihlášení"}
 
-@view_config(route_name='signup', renderer='C:\\Users\\Martin\\Desktop\\ŠR 25-26\\Tools\\ChronoQuest\\chrono_quest\\src\\chronoQuest\\templates\\signup.jinja2', request_method=('GET','POST'))
+@view_config(route_name='signup', renderer='chronoQuest:templates/signup.jinja2', request_method=('GET','POST'))
 def signup_view(request):
     # TODO: zde později zpracujeme POST (vytvoření účtu)
     return {"title": "Registrace"}
